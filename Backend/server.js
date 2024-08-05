f3b8a044c20e8f209508e2ae0eb4735217b3383c
LathithaKunana <lathitha.kunana@younglings.africa>
2024-08-05 14:19:46 +0200

    set timeout reduced

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const port = 3000; // You can use any available port
 
 // Apply CORS middleware globally
 app.use(cors({
-    origin: 'https://image-swapper-frontend.vercel.app' 
+    origin: 'http://localhost:5173' 
   }));
 
 app.use(express.json());
@@ -94,14 +94,14 @@ app.post('/api/face-swap', upload.fields([{ name: 'target_image' }, { name: 'swa
           },
         });
 
-        res.setHeader('Access-Control-Allow-Origin', 'https://image-swapper-frontend.vercel.app');
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
         console.log('Result API Response:', resultResponse.data);
         res.json(resultResponse.data);
       } catch (error) {
         console.error('Error retrieving result:', error);
         res.status(500).json({ error: 'An error occurred while retrieving the result' });
       }
-    }, 2000);
+    }, 1000);
   } catch (error) {
     console.error('Error processing request:', error);
     res.status(500).json({ error: 'An error occurred' });
